eecedade9bbca378bab86f1b68d0384135d8fe13
Ivan Savov <ivan.savov@gmail.com>
2018-01-16 09:57:46 -0500

    Make studio initialization wait for DB to come up before running

--- dockerfiles/scripts/studio/entrypoint.sh
+++ dockerfiles/scripts/studio/entrypoint.sh
@@ -2,37 +2,55 @@
 # Initialize Studio instance (assumes all commands are idempotent) Are they?
 set -e
 
-if [ ! -z ${STUDIO_FIXTURES} ] ; then
-    echo "Loading" "$STUDIO_FIXTURES" "TODO<<<<<<"
-fi
-
-
-echo "\n\n\nRUNNING nslookup studiodb >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo nslookup studiodb
-
-
-
-
 
 cd /contentcuration/contentcuration
 
+
+echo "\n\n\nRUNNING nslookup studiodb >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+nslookup studiodb
+
+
+# Wait for studiodb host to come up
+################################################################################
+until psql studiodb -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - continuing"
+
+
+
+
 # Initialize application: migrate, collectstatic, crowdin messages
 ################################################################################
-# migrate
-# python manage.py makemigrations
-python manage.py migrate || true
-# collectstatic
-python manage.py collectstatic --noinput
-python manage.py collectstatic_js_reverse
-python manage.py loadconstants
+if [ "x$INITIALIZE_STUDIO" = 'xon' ]; then
 
-#    # crowdin messages
-#    ls -l crowdin-cli.jar || curl -L https://storage.googleapis.com/le-downloads/crowdin-cli/crowdin-cli.jar -o crowdin-cli.jar
-#    java -jar crowdin-cli.jar download
-#    python manage.py compilemessages
+    # migrate
+    python manage.py migrate || true
+    # python manage.py makemigrations
 
-# 
-python manage.py calculateresources --init
+    # collectstatic
+    python manage.py collectstatic --noinput
+    python manage.py collectstatic_js_reverse
+    python manage.py loadconstants
+
+    # rsrc sizes
+    python manage.py calculateresources --init
+
+    #    # crowdin messages
+    #    ls -l crowdin-cli.jar || curl -L https://storage.googleapis.com/le-downloads/crowdin-cli/crowdin-cli.jar -o crowdin-cli.jar
+    #    java -jar crowdin-cli.jar download
+    #    python manage.py compilemessages
+
+fi
+
+
+
+
+if [ ! -z ${STUDIO_FIXTURES} ] ; then
+    echo "Loading" "$STUDIO_FIXTURES" "TODO<<<<<<"
+fi
 
 
 
